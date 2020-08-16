a1f0b165287d9cd7af87986c79f3cb1409f8bd00
Jamie <jamie_arnold@msn.com>
2020-08-16 20:06:09 +0100

    Super Basic Curse Meter

---
+++ Assets/proj/scripts/PlayerCurse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCurse : MonoBehaviour
+{
+    public static int CurseMeter;
+
+
+    void Start()
+    {
+
+        CurseMeter = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            CurseMeter += 10;
+        }
+    }
+}
