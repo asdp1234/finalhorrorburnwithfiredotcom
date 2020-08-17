0b2ede5950a8f711a71918e209bf09b1e1c985f3
connor <connorswannell@me.com>
2020-08-17 22:55:32 +0100

    Auto stash before revert of "choice code added "

---
+++ Assets/proj/scripts/Choice/listofchoices.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class listofchoices : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] list;
+    // Start is called before the first frame update
+  
+    public GameObject Getlist(int i)
+    {
+        return list[i];
+    }
+}
