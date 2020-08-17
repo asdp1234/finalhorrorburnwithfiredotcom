d73cc5b76d388427d982c85824a2079023b2edf4
Jamie <jamie_arnold@msn.com>
2020-08-17 14:49:45 +0100

    To you

---
+++ Assets/proj/scripts/PickUpScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+
+    public bool  Sword;
+    public bool  Key;
+    public bool  Flask;
+
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        
+        if (col.gameObject.tag.Equals("player"))
+        {
+            if (Sword)
+                Debug.Log("The player has the sword, no one is safe");
+
+            if (Key)
+            {
+                Debug.Log("I got the key, I got the secret");
+            }
+
+            if (Flask)
+                Debug.Log("Ladies and gentlemen, we got him");
+        }
+
+    }
+}
--- Assets/proj/scripts/PlayerCurse.cs
+++ Assets/proj/scripts/PlayerCurse.cs
@@ -45,5 +45,5 @@ public class PlayerCurse : MonoBehaviour
     {
         CurseMeter += 10;
     }
-
+        
 }
