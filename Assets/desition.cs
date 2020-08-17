86ca7f186928a04935966d5866b3bcba32972598
connor <connorswannell@me.com>
2020-08-17 22:47:43 +0100

    choice code added

---
+++ Assets/desition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desition : MonoBehaviour
+{
+
+    listofchoices list;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        list = FindObjectOfType<listofchoices>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag.Equals("Player"))
+        {
+            if (this.tag.Equals("One"))
+            {
+                Debug.Log("eyes");
+                list.Getlist(0).SetActive(true);
+                Time.timeScale = 0;
+            }
+            if (this.tag.Equals("Two"))
+            {
+                Debug.Log("red");
+            }
+        }
+        
+    }
+}
