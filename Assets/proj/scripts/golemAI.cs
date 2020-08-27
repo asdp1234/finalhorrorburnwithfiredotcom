3f92f5649f7c7fe184bd49b04fdb3150062cc493
connor <connorswannell@me.com>
2020-08-27 20:09:39 +0100

    base ai code

---
+++ Assets/proj/scripts/golemAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class golemAI : MonoBehaviour
+{
+    bool idelstate = false;
+    [SerializeField]
+    Transform point1, point2;
+    [SerializeField]
+    float speed  = 3;
+    bool left = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!idelstate)
+        {
+            float step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, point2.position, step);
+            if (Vector3.Distance(transform.position, point2.position) < 0.001f)
+            {
+                left = true;
+                //transform.position = Vector3.MoveTowards(transform.position, point1.position, step);
+            }
+            if (Vector3.Distance(transform.position, point1.position) < 0.001f)
+            {
+                left = false;
+                //transform.position = Vector3.MoveTowards(transform.position, point1.position, step);
+            }
+        }
+        else
+        {
+
+        }
+    }
+}
